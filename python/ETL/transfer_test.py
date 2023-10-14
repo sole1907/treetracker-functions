de973fe8e74204f34c7d968381f1c436555dfb07
Xinyi He <xyh@Leviathan-No3.local>
2023-10-14 01:46:52 -0700

    change test url

--- python/ETL/transfer_test.py
+++ python/ETL/transfer_test.py
@@ -8,12 +8,15 @@ class Test_Transfer(unittest.TestCase):
         import os
         print("transfer data")
         # read env variables DB_URL
-        DB_URL = os.environ['DB_URL']
+        DB_SOURDE_URL = os.environ['DB_SOURDE_URL']
+        DB_DESTINATION_URL  = os.environ[' DB_DESTINATION_URL ']
+        
         # need SRC DB_URL and TARGET DB_URL
         # need organization id
-        print("DB_URL:", DB_URL)
-        src_conn = psycopg2.connect(DB_URL, sslmode='require')
-        dest_conn = psycopg2.connect(DB_URL, sslmode='require')
+        print("DB_SOURDE_URL:", DB_SOURDE_URL)
+        print("DB_DESTINATION_URL:",  DB_DESTINATION_URL)
+        src_conn = psycopg2.connect(DB_SOURDE_URL, sslmode='require')
+        dest_conn = psycopg2.connect( DB_DESTINATION_URL , sslmode='require')
         transfer(dest_conn,src_conn, 11, action = False)
 
         return 
